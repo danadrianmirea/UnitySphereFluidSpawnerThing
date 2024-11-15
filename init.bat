ed3407b3e37e60ed91df986ee1c27af69a3f647a
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-15 23:43:31 +0200

    remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# UnitySphereFluidSpawnerThing" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/UnitySphereFluidSpawnerThing.git
-git push -u origin main
